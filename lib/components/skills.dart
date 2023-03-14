b6269971cf9238dd7e4d81e5d217f5f5fb142ca9
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-03-14 14:55:29 +0530

    skills section added

--- lib/components/skills.dart
+++ lib/components/skills.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Skills extends StatelessWidget {
   const Skills({super.key});
@@ -8,31 +9,169 @@ class Skills extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Wrap(
+      spacing: 5.0,
+      runSpacing: 5.0,
+      alignment: WrapAlignment.center,
       children: [
+        Divider(),
         Container(
+          width: context.screenWidth < 900
+              ? context.screenWidth * 0.9
+              : ((context.screenWidth * 0.8) / 3),
           padding: EdgeInsets.symmetric(vertical: 15.0),
           alignment: Alignment.center,
           child: Text(
             "My Skills",
             style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
           ),
-        )
-        //
-
+        ),
         Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
           child: Column(
-            children: [
-              Text("coding",
-              ),
-
-              Divider(),
-              Chip(label: Text("Python",
-              ),
-              
+            children: const [
+              Chip(
+                label: Text(
+                  "Python",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
               ),
             ],
           ),
-        )
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "Django",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "Html/Css",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "Git",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "Github",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "Flutter",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "DRF",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          // width: context.screenWidth < 900
+          //     ? context.screenWidth * 0.9
+          //     : ((context.screenWidth * 0.8) / 3),
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: const [
+              Chip(
+                label: Text(
+                  "Bootstrap",
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                side: BorderSide(color: Colors.black),
+                backgroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+
+        Divider(),
+        //
       ],
     );
   }
