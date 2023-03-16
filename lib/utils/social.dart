b83d8ecd964a6daa767f1fb729eb6702642c7a03
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-03-16 14:16:55 +0530

    redirect aded

--- lib/utils/social.dart
+++ lib/utils/social.dart
@@ -1,51 +1,119 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/placeholder.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// class Social extends StatefulWidget {
+//   const Social({super.key});
+
+//   @override
+//   State<Social> createState() => _SocialState();
+// }
+
+// class _SocialState extends State<Social> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Row(
+//           // padding: const EdgeInsets.all(8.0),
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: const [
+//             Center(
+//               child: Padding(
+//                 // padding: EdgeInsets.all(8.0),
+//                 padding: const EdgeInsets.only(left: 20.0, top: 20.0),
+//                 child: Center(
+//                   child: FaIcon(
+//                     FontAwesomeIcons.instagram,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             Padding(
+//               // padding: EdgeInsets.all(8.0),
+//               padding: const EdgeInsets.only(left: 30.0, top: 20.0),
+//               child: Center(
+//                 child: FaIcon(
+//                   FontAwesomeIcons.google,
+//                 ),
+//               ),
+//             ),
+//             Padding(
+//               // padding: EdgeInsets.all(8.0),
+//               padding:
+//                   const EdgeInsets.only(left: 30.0, right: 20.0, top: 20.0),
+//               child: Center(
+//                 child: FaIcon(
+//                   FontAwesomeIcons.squarePhone,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Social extends StatefulWidget {
-  const Social({super.key});
+  const Social({Key? key});
 
   @override
   State<Social> createState() => _SocialState();
 }
 
 class _SocialState extends State<Social> {
+  final String instagramUrl = 'https://www.instagram.com/';
+  final String googleUrl = 'https://www.google.com/';
+  final String phoneNumber = 'tel:1234567890';
+
+  void _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
-          // padding: const EdgeInsets.all(8.0),
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Center(
+          children: [
+            GestureDetector(
+              onTap: () => _launchUrl(instagramUrl),
               child: Padding(
-                // padding: EdgeInsets.all(8.0),
                 padding: const EdgeInsets.only(left: 20.0, top: 20.0),
-                child: Center(
-                  child: FaIcon(
-                    FontAwesomeIcons.instagram,
-                  ),
+                child: FaIcon(
+                  FontAwesomeIcons.instagram,
                 ),
               ),
             ),
-            Padding(
-              // padding: EdgeInsets.all(8.0),
-              padding: const EdgeInsets.only(left: 30.0, top: 20.0),
-              child: Center(
+            GestureDetector(
+              onTap: () => _launchUrl(googleUrl),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30.0, top: 20.0),
                 child: FaIcon(
                   FontAwesomeIcons.google,
                 ),
               ),
             ),
-            Padding(
-              // padding: EdgeInsets.all(8.0),
-              padding:
-                  const EdgeInsets.only(left: 30.0, right: 20.0, top: 20.0),
-              child: Center(
+            GestureDetector(
+              onTap: () => _launchUrl(phoneNumber),
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 30.0, right: 20.0, top: 20.0),
                 child: FaIcon(
                   FontAwesomeIcons.squarePhone,
                 ),
