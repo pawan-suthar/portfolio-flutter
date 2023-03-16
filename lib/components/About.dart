8242c59272ef6687680b8cde3e2417fd766ff6d6
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-03-16 15:22:32 +0530

    add redirect i about page on gihuband linkedin but

--- lib/components/About.dart
+++ lib/components/About.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'dart:io';
 import '../utils/social.dart';
 
 class About extends StatefulWidget {
@@ -12,6 +13,30 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _downloadResume() async {
+    const url =
+        'https://drive.google.com/file/d/1fAevG4bTt3f3BHCA3GhPMkZG2ZZxX5l7/view?usp=sharing'; // replace with your resume URL
+    final response = await HttpClient().getUrl(Uri.parse(url));
+    final bytes = await response.close();
+    await File('resume.pdf').writeAsBytes((await bytes.toList()).cast<int>());
+  }
+
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -45,32 +70,24 @@ class _AboutState extends State<About> {
           alignment: WrapAlignment.center,
           spacing: 8.0,
           runSpacing: 8.0,
-          children: const [
+          children: [
             Padding(
               padding: EdgeInsets.all(8.0),
-              child: Chip(
-                label: Text(
-                  "Resume",
+              child: InkWell(
+                onTap: () => _downloadResume(),
+                child: Chip(
+                  label: Text(
+                    "Resume",
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14.0,
+                  ),
+                  backgroundColor: Colors.transparent,
+                  padding: EdgeInsets.all(8.0),
                 ),
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14.0,
-                ),
-                backgroundColor: Colors.transparent,
-                padding: EdgeInsets.all(8.0),
               ),
             ),
-            // Chip(
-            //   label: Text(
-            //     "python developer",
-            //   ),
-            //   labelStyle: TextStyle(
-            //     color: Colors.white,
-            //     fontSize: 14.0,
-            //   ),
-            //   backgroundColor: Colors.green,
-            //   padding: EdgeInsets.all(8.0),
-            // ),
           ],
         ),
         Divider(),
@@ -79,15 +96,22 @@ class _AboutState extends State<About> {
         Center(
           child: Container(
             child: Row(children: [
-              const Padding(
-                padding: EdgeInsets.all(12.0),
-                child: Card(
-                  child: FaIcon(
-                    FontAwesomeIcons.gitAlt,
-                    size: 20.0,
+              GestureDetector(
+                onTap: () {
+                  String url = "https://github.com/pawan-suthar";
+                  launchURL(url);
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Card(
+                    child: FaIcon(
+                      FontAwesomeIcons.gitAlt,
+                      size: 20.0,
+                    ),
                   ),
                 ),
               ),
+
               //
               Center(
                 child: Column(
@@ -108,23 +132,29 @@ class _AboutState extends State<About> {
         Center(
           child: Container(
             child: Row(children: [
-              const Padding(
-                padding: EdgeInsets.all(12.0),
-                child: Card(
-                  child: FaIcon(
-                    FontAwesomeIcons.linkedin,
-                    size: 20.0,
+              GestureDetector(
+                onTap: () {
+                  String url = "https://www.linkedin.com/in/pawansuthar1537/";
+                  launchURL(url);
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Card(
+                    child: FaIcon(
+                      FontAwesomeIcons.gitAlt,
+                      size: 20.0,
+                    ),
                   ),
                 ),
               ),
-              //
 
+              //
               Center(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
                     Text(
-                      "linkedin",
+                      "Linkedin",
                     ),
                     Text(
                       "pawansuthar1537",
